827d5fc6f13b127f08dba0eea10b5e2442114017
Michał <mwarioxin@gmail.com>
2019-01-09 22:54:41 +0100

    fix follow commit

--- game/Scripts/GameOver.gd
+++ game/Scripts/GameOver.gd
@@ -9,12 +9,14 @@ func init():
 func _on_NewGameButton_pressed():
 	Global.restart_stats()
 	get_tree().paused = false
+	$CanvasLayer/CenterContainer/NinePatchRect/VBoxContainer/CenterContainer/NewGameButton/Click.play()
 	Transition.fade_to(Global.Level)
 	#et_tree().change_scene(Global.Level)
 
 func _on_MenuButton_pressed():
 	get_tree().paused = false
 	#print("HALOOO")
+	$CanvasLayer/CenterContainer/NinePatchRect/VBoxContainer/CenterContainer3/MenuButton/Click
 	Transition.fade_to(Global.SplashScreen)
 	#get_tree().change_scene(Global.SplashScreen)
 
